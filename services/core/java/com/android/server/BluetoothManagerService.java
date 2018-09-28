2ff6f50e2a1776c246074de2f889eed6d8d21fbd
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2018-09-28 14:45:35 +0530

    Bluetooth: Remove bind timeout message on service connection
    When user switch attempted multiple times message handler
    taking time to deliver messages to bluetooth handler.
    
    If MESSAGE_TIMEOUT_BIND ,MESSAGE_ENABLE and MESSAGE_BLUETOOTH_SERVICE_CONNECTED
    queued and executed , then bluetooth manager try to bind to adapter service again,
    will start second bluetooth process.
    
    Hence remove bind timeout message immediately on bluetooth service connection
    
    Change-Id: I20373351a74526efcd097e4bb3b019f96b8d65d5
    CRs-Fixed: 2321665
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -1660,6 +1660,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             Message msg = mHandler.obtainMessage(MESSAGE_BLUETOOTH_SERVICE_CONNECTED);
             if (name.equals("com.android.bluetooth.btservice.AdapterService")) {
                 msg.arg1 = SERVICE_IBLUETOOTH;
+                mHandler.removeMessages(MESSAGE_TIMEOUT_BIND);
             } else if (name.equals("com.android.bluetooth.gatt.GattService")) {
                 msg.arg1 = SERVICE_IBLUETOOTHGATT;
             } else {
@@ -1963,9 +1964,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                             continueFromBleOnState();
                             break;
                         } // else must be SERVICE_IBLUETOOTH
-
-                        //Remove timeout
-                        mHandler.removeMessages(MESSAGE_TIMEOUT_BIND);
 
                         mBinding = false;
                         mBluetoothBinder = service;
