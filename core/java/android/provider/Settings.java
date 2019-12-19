b022ac39e34af88e0e5ea48a668e8113fa78b4b0
00day0 <therandomuser11@gmail.com>
2019-12-19 23:29:50 +0100

    [SQUASH] base: Tint QS Tile with accent color (1/2)
    * Tinted QS Tile stuffs (icon, background, label)
    * Using bolder font
    
    Adjust QS tile background and icon color
    
    + make qs tile icon smaller by 1dp
    + reduce qs tile vertical margin
    
    SystemUI: Add a toggle for the new QS tint [1/2]
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I612d102b45127f2289023ccf4920caad511a1fca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4978,6 +4978,12 @@ public final class Settings {
          */
         public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
 
+	/**
+         * Whether to use new QS panel bg tint or not
+         * @hide
+         */
+        public static final String QS_PANEL_BG_USE_NEW_TINT = "qs_panel_bg_use_new_tint";
+
         /**
          * @hide
          */
@@ -5141,6 +5147,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC);
             PRIVATE_SETTINGS.add(INCALL_FEEDBACK_VIBRATE);
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
+   	    PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
         }
 
         /**
